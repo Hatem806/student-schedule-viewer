ec1cd30f169edcfe32284999e9e3427e424b4ae9
Hatem Abdelsattar Elsayed <hatemabdelsattar@gmail.com>
2025-05-28 01:11:24 +0300

    chore: remove unused ApiStrategy interface and MockApiStrategy implementation along with their tests

--- src/app/core/strategies/api-strategy.interface.ts
+++
@@ -1,6 +0,0 @@
-import type { Observable } from "rxjs"
-import type { ScheduleRequest, ScheduleResponse } from "../models/schedule.model"
-
-export interface ApiStrategy {
-  getStudentSchedule(request: ScheduleRequest): Observable<ScheduleResponse>
-}
